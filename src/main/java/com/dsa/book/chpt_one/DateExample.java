3885f8bf69688c29bb12606bf9b8cf2cd325a55f
Evi Owhotemu <evicoach@gmail.com>
2023-04-01 14:31:36 +0100

    Updated the readme file

---
+++ src/main/java/com/dsa/book/chpt_one/DateExample.java
@@ -0,0 +1,10 @@
+package main.java.com.dsa.book.chpt_one;
+
+import java.util.Date;
+
+public class DateExample {
+    public static void main(String[] args){
+        Date today = new Date();
+        System.out.println("Today's date is " + today);
+    }
+}
